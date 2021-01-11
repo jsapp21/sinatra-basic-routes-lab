3a78e1b0303a160853933634e4b33751587945b0
James Sapp <jameseisapp@gmail.com>
2021-01-11 11:02:56 -0600

    commit tree 6dd9e11c86771019670e122fef4fe42aab800768

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is James"
+    end
+
+    get '/hometown' do
+        "My hometown is Jacksonville, IL"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Innerbloom by Rufus du Sol"
+    end
+
 end
